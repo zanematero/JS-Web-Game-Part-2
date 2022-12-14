403cfc9fa785bd2866a7f18eb63cfdbf61656a0e
Zane Matero <zanematero@Zanes-MacBook-Pro.local>
2022-12-14 13:43:38 -0800

    added inventory mechanic

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ function newImage(url, left, bottom){
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function(){
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
 
+let inventory = document.createElement('div')
+
 function newInventory(){
-    let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
